6d16bbc686086d7c8eabaddf3e4c8da1b310ff6a
Chase Sillevis <chase@sillevis.net>
2015-09-23 19:12:13 +0200

    qlimax seeker

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var data = JSON.parse(fs.readFileSync('./site.json'));
-processUrls(data.urlset.url);
+'use strict';
 
-function processUrls(data) {
-	data.forEach(function(v, i) {
-		var options = {
-		    url: v.loc,
-		    headers: {
-		        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
-		    }
-		};
-		request(options, function(err, resp, body) {
-	        if (err) {
-	        	console.log('Error? ', resp);
-	            return console.log(err);
-            }
+var request = require('request-promise');
+var Promise = require('bluebird');
 
-            if(body.indexOf('pebble') > 0) {
-            	$ = cheerio.load(body);
-            	console.log($('title').text());
-	        	return console.log('FOUND!!!!!!!!!! ', options.url);
-            }
-            else {
-        		return false;
-        	}
-	    });
-	});
+var endpoint = 'https://frontoffice.paylogic.nl/?event_id=',
+    startpoint = 108000,
+    urls = [],
+    resultUrls = [],
+    keyword = 'qlimax';
+
+for(var i = 0 ; i <= 1000 ; i++) {
+  urls.push(endpoint + (startpoint + i));
 }
+
+Promise.all(urls).each(function(url) {
+  return request({
+      url: url,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
+      }
+  }).then(function(body) {
+    if(body.toLowerCase().indexOf(keyword) > 0) {
+      // do something cool
+      console.log('FOUND', keyword, '!!!!');
+      resultUrls.push(url);
+    }
+  }).catch(function(err) {
+    //console.error('[ERROR]', err);
+  });
+}).then(function() {
+  console.log('results: ', resultUrls);
+});
